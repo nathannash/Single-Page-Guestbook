37bfca49e9eb56cf8f3acbabfc6099c49504628b
Nathan <mcknathannash@gmail.com>
2013-07-30 14:45:58 +0800

    added some UI styles

--- index.php
+++ index.php
@@ -2,21 +2,15 @@
 <html lang="en">
     <head>
 	    <title>Exercise 2: Guestbook</title>
-        <link rel="stylesheet" href="lib/css/bootstrap.css" type="text/css" media="screen" charset="utf-8"> 
+        <link rel="stylesheet" href="lib/css/bootstrap.css" type="text/css" media="screen" charset="utf-8">
+        <link rel="stylesheet" href="lib/css/style.css" type="text/css" media="screen" charset="utf-8">  
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>		
 		<script type="text/javascript" src="lib/js/bootstrap.js"></script>
-		<!-- any custom styling-->
-		<style type="text/css">
-			#wrapper {
-				margin:0 auto;
-				padding:100px 0 0 0;
-				width:960px;
-			}
-			
-			.errorText, .required {
-				color:#F00;
-			}
-		</style>
+		<script type="text/javascript">
+		$(document).ready(function(){
+			$(".comment:odd").addClass("alternateBG");
+		});
+		</script>
     </head>
 <body>
 	<section id="wrapper">
@@ -27,8 +21,8 @@
 					$FormName = "guest_name";					
 					$FormEmail = "email_address";
 					$FormMessage = "post_message";	
-					$FormImage = "post_image";
-					$Name = $Email = $Message = "";
+					$FormAttachment = "picture_file";
+					$Name = $Email = $Message = $Attachment = "";
 				?>
 				<form id="comment-form" action="<?php htmlspecialchars($_SERVER['PHP_SELF'])?>" enctype="multipart/form-data" method="post">
 					<!--name-->
@@ -37,19 +31,20 @@
 					<!--email-->
 					<label for="<?php echo $FormEmail; ?>"><strong><span class="required">*</span>Email:</strong></label>
 					<input id="post-email" class="noerror" type="text" name="<?php echo $FormEmail; ?>" value="<?php if(isset($_POST['email_address'])) echo $_POST['email_address']; ?>">	
-					<!--website-->
 					<!--message-->
 					<label for="<?php echo $FormMessage; ?>"><strong><span class="required">*</span>Message:</strong></label>
 					<textarea id="post-message" class="noerror" name="<?php echo $FormMessage; ?>" ><?php if(isset($_POST["post_message"])) echo $_POST["post_message"]?></textarea>	
 					<!--attachment-->
-					<label for="<?php echo $FormImage; ?>"><strong>Upload Picture:</strong></label>
-					<input id="post-image" class="noerror" name="<?php echo $FormImage; ?>" type="file">			
-					<!--submit-->
+					<label for="<?php echo $FormAttachment; ?>"><strong>Upload Picture:</strong></label>
+					<input id="post-image-size" name="size_constraint" type ="hidden">
+					<input id="post-image" class="well" name="<?php echo $FormAttachment; ?>" type="file">			
+					<!--submit:: hidden fields enforce file size and check if the form has been submitted-->
+				    <input type="hidden" name="MAX_FILE_SIZE" value="2097152">
 				    <input type="hidden" name="submitted" value="1">  
-					<input type="submit" class="noerror" name="submit" value="Let 'er rip" />
+					<input class="submit" type="submit" name="submit"  value="Let 'er rip" />
 				</form>
 			</div>
-			<div class="span8">
+			<div class="span8 wall">
 				<h2>Guestbook</h2>
 				<?php
 								
@@ -64,7 +59,8 @@
 						    global $Name;
 							$Name = $_POST["guest_name"];
 						} else {
-							echo '<script type="text/javascript">
+							echo 
+							'<script type="text/javascript">
 								$(document).ready(function(){
 									$("#post-name").removeClass("noerror").addClass("error");
 								});
@@ -75,7 +71,8 @@
 							global $Email;
 							$Email = $_POST["email_address"];
 						} else {
-							echo '<script type="text/javascript">
+							echo 
+							'<script type="text/javascript">
 								$(document).ready(function(){
 									$("#post-email").removeClass("noerror").addClass("error");
 								});
@@ -86,22 +83,47 @@
 							global $Message;
 							$Message = $_POST["post_message"];
 						} else {
-							echo '<script type="text/javascript">
+							echo 
+							'<script type="text/javascript">
 								$(document).ready(function(){
 									$("#post-message").removeClass("noerror").addClass("error");
 								});
 							</script>';							
 						}
 						
+						//Attachment Validation
+						$FileType = $_FILES['picture_file']['type'];
+						$FileSize = $_FILES['picture_file']['size'];
+						$FileName = $_FILES['picture_file']['name'];
+						$Dir = "uploads/";
+						
+						//If user uploads file...
+						if($_FILES){
+							if (($FileType == "image/gif") 
+						    || ($FileType == "image/jpg") 
+							|| ($FileType == "image/jpeg") 
+							|| ($FileType == "image/png") 
+							&& ($FileSize < 2097152)) {
+								//Make public directory 
+								if(move_uploaded_file($_FILES['picture_file']['tmp_name'], $Dir . $FileName === FALSE)){
+									echo "<div class='alert alert-danger'> Could not move uploaded file to \"uploads" . htmlentities($FileName) . "\"</div>\n";
+								} else {
+									mkdir($Dir, 0777);
+									chmod($Dir . $FileName, 0644); 
+									echo "<div class='alert alert-success'>Successfully uploaded \"uploads/" . htmlentities($FileName) . "\"</div>\n";
+								}
+							}								
+						}
+							
 						//If all required fields are set publish comment
 						if(isset($Name) && isset($Email) && isset($Message)){
 							publishComment();
 						} else {
-							echo "<strong class='errorText'>Please complete all required fields.</strong>";							
+							echo "<div class='alert alert-danger'>Please complete all required fields. Or <strong><a class='alert-link' href='/nnash_ex2/'>start over</a></strong></div>";							
 						}	
 					}
 					
-					//Page content
+					//Load comments into page 
 					displayComments();					
 					
 					function publishComment(){
@@ -109,12 +131,11 @@
 						global $Name;
 						global $Email;
 						global $Message;
-						global $File;
-						$Date = date('o F d');
+						global $Attachment;
+						$Date = date('o F d, g:i A'); //YYYY MM DD, 00:00 AM/PM
 						
 						//Format comment 
-						$Comment = "<p>" . $Message . "</p>" . "<hr />" . "<p>By " . $Name . " on " . $Date . "</p>\n";
-						
+						$Comment = "<div class='comment'><p>" . $Message . "</p>" . "<hr />" . "<p>By <a href=mailto:" . $Email . ">" . $Name . "</a> on "  . $Date . "</p></div>\n";
 						//VARs to write the file 
 						$Dir = "comments/";
 						$File = 'comments/comments.txt';
@@ -132,10 +153,16 @@
 						}							    						
 					}
 					
-					//If the form has 'ever' been submitted load comments file
 					function displayComments(){
 						if(is_dir("comments/")){
-							include("comments/comments.txt"); 
+							$Comments = file("comments/comments.txt");
+							$CommentLines = count($Comments);
+							//Display comments with loop
+							for($i = 0; $i < $CommentLines; ++$i){
+								echo $Comments[$i];
+							}
+						} else {
+							echo "<div class='alert alert-info'>You're the <strong>first one</strong> here! Leave a comment!</div>";
 						}
 					}					
 			 ?>
@@ -143,4 +170,4 @@
 		</div>
 	</section>
 </body>
-</html> 
+</html>
